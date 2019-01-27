4c544161b0db52f648b210bd1abc2be85a254b5f
xgreyskiesx <greysdawn@gmail.com>
2019-01-27 01:57:50 -0500

    Adding role functions

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-:main
-cls
-supervisor -e .js,.json,.sqlite -w bot.js,strings.json,config.json,help.js -n success bot.js
-pause
-set /p yn=Would you like to restart?
-if %yn%==y goto main
-if %yn%==no exit
