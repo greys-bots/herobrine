b90619b36cea8727a4a10bbdb606c8d8bff4eeee
greysdawn <greysdawn@gmail.com>
2019-02-24 01:23:14 -0500

    Adding ecosystem file for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    name: 'Herobrine',
+    script: 'bot.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: ["commands/","bot.js","config.json","strings.json","utilities.js"],
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
