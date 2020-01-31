3cdd894c3967b64b87c100c02d4fa9b0488c0ece
greysdawn <greysdawn@gmail.com>
2020-01-31 03:00:08 -0500

    Fix disable/enable command, add module aliases

--- commands/Customization/disable.js
+++ commands/Customization/disable.js
@@ -1,58 +1,69 @@
 module.exports = {
-	help: ()=> "Disables a command/module or a command's subcommands.",
-	usage: ()=> [" [command/module] <subcommand> - disables given command or its subcommand",
-				" view - lists disabled commands"],
+	help: ()=> "Disables a command/module or a command's subcommands",
+	usage: ()=> [" - Lists disabled commands",
+				 " [command/module] <subcommand> - Disables given command or its subcommand"],
 	execute: async (bot, msg, args) => {
-		if(!args[0]) return bot.commands.disable.subcommands.view.execute(bot, msg, args);
-		if(args[0] == "disable" || args[0] == "enable") return msg.channel.createMessage("You can't disable or enable this command.");
+		if(!args[0]) {
+			var cfg = await bot.utils.getConfig(bot, msg.guild.id);
+			if(!cfg || !cfg.disabled || (cfg.disabled && !cfg.disabled.commands && !cfg.disabled.levels)) return msg.channel.createMessage('No config found for this server')
+			
+			return msg.channel.createMessage({embed: {
+				title: "Disabled Functions",
+				fields: [
+					{name: "Commands", value: cfg.disabled.commands[0] ? cfg.disabled.commands.sort().join("\n") : "(none)"},
+					{name: "Levels", value: cfg.disabled.levels ? "Yes" : "No"}
+				]
+			}})
+		}
+
+		if(["disable", "enable"].includes(args[0].toLowerCase())) return msg.channel.createMessage("You can't disable or enable this command");
 		var cfg = await bot.utils.getConfig(bot, msg.guild.id);
 		var dis;
-		if(!cfg) return msg.guild.createMessage("No config registered for this server.");
+		if(!cfg) return msg.guild.createMessage("No config registered for this server");
 		if(!cfg.disabled) dis = {modules: [], commands: [], levels: false};
 		else dis = cfg.disabled;
 		if(!dis.commands) dis.commands = [];
 		if(!dis.levels) dis.levels = false;
 		console.log(dis)
-		var cmd;
 		try {
-			cmd = await bot.parseCommand(bot, msg, args);
+			var {command} = await bot.parseCommand(bot, msg, args);
 		} catch (e) {
-			cmd = undefined;
+			var command = undefined;
 		}
-		if(cmd) {
-			var disabled = await bot.utils.isDisabled(bot, msg.guild.id, cmd[0], cmd[2]);
+		if(command) {
+			var disabled = await bot.utils.isDisabled(bot, msg.guild.id, command, command.name);
 			if(disabled) {
-				return msg.channel.createMessage("Module already disabled.")
+				return msg.channel.createMessage("Command already disabled")
 			} else {
-				dis.commands.push(cmd[2])
+				dis.commands.push(command.name)
 				var success = await bot.utils.updateConfig(bot, msg.guild.id, {disabled: dis});
 				if(success) {
 					msg.channel.createMessage("Disabled!")
 				} else {
-					msg.channel.createMessage("Something went wrong.");
+					msg.channel.createMessage("Something went wrong");
 				}
 			}
 		} else {
-			cmd = args[0].toLowerCase()
+			var cmd = args.join(" ").toLowerCase()
 			if(["levels", "levelup", "levelups"].includes(cmd)) {
 				dis.levels = true;
 				var success = await bot.utils.updateConfig(bot, msg.guild.id, {disabled: dis});
 				if(success) {
 					msg.channel.createMessage("Disabled!")
 				} else {
-					msg.channel.createMessage("Something went wrong.");
+					msg.channel.createMessage("Something went wrong");
 				}
-			} else if(bot.modules[cmd]) {
-				var cmds = Object.keys(bot.commands).filter(c => bot.commands[c].module && bot.commands[c].module == cmd);
-				cmds.forEach(c => dis.commands.push(c))
+			} else if(bot.modules.get(bot.mod_aliases.get(cmd))) {
+				var mod = bot.modules.get(bot.mod_aliases.get(cmd));
+				dis.commands = dis.commands.concat(mod.commands.map(c => c.name));
 				var success = await bot.utils.updateConfig(bot, msg.guild.id, {disabled: dis});
 				if(success) {
 					msg.channel.createMessage("Disabled!")
 				} else {
-					msg.channel.createMessage("Something went wrong.");
+					msg.channel.createMessage("Something went wrong");
 				}
 			} else {
-				return msg.channel.createMessage("Could not disable: "+e);
+				return msg.channel.createMessage("Command/module not found");
 			}
 		}
 	},
@@ -67,15 +78,6 @@ module.exports.subcommands.view = {
 	help: ()=> "View currently disabled commands and modules.",
 	usage: ()=> [" - Views the disabled config for the server"],
 	execute: async (bot, msg, args) => {
-		var cfg = await bot.utils.getConfig(bot, msg.guild.id);
-		if(!cfg || !cfg.disabled || (cfg.disabled && !cfg.disabled.commands && !cfg.disabled.levels)) return msg.channel.createMessage('No config found for this server.')
 		
-		msg.channel.createMessage({embed: {
-			title: "Disabled Functions",
-			fields: [
-				{name: "Commands", value: cfg.disabled.commands[0] ? cfg.disabled.commands.sort().join("\n") : "(none)"},
-				{name: "Levels", value: cfg.disabled.levels ? "Yes" : "No"}
-			]
-		}})
 	}
 }
--- commands/Customization/enable.js
+++ commands/Customization/enable.js
@@ -1,34 +1,33 @@
 module.exports = {
-	help: ()=> "Enables a command/module or a command's subcommands.",
-	usage: ()=> [" [command/module] <subcommand> - enables given command or its subcommand"],
+	help: ()=> "Enables a command/module or a command's subcommands",
+	usage: ()=> [" [command/module] <subcommand> - Enables given command or its subcommand"],
 	execute: async (bot, msg, args) => {
 		if(!args[0]) return msg.channel.createMessage("Please provide a command or module to enable.");
-		if(args[0] == "disable" || args[0] == "enable") return msg.channel.createMessage("You can't disable or enable this command.");
+		if(["disable", "enable"].includes(args[0].toLowerCase())) return msg.channel.createMessage("You can't disable or enable this command");
 		var cfg = await bot.utils.getConfig(bot, msg.guild.id);
-		if(!cfg) return msg.guild.createMessage("No config registered for this server.");
-		if(!cfg.disabled) return msg.guild.createMessage("Nothing is disabled in this server.");
+		if(!cfg) return msg.guild.createMessage("No config registered for this server");
+		if(!cfg.disabled) return msg.guild.createMessage("Nothing is disabled in this server");
 		var dis = cfg.disabled;
-		var cmd;
 		try {
-			cmd = await bot.parseCommand(bot, msg, args);
+			var {command} = await bot.parseCommand(bot, msg, args);
 		} catch (e) {
-			cmd = undefined;
+			var command = undefined;
 		}
-		if(cmd) {
-			var disabled = await bot.utils.isDisabled(bot, msg.guild.id, cmd[0], cmd[2]);
+		if(command) {
+			var disabled = await bot.utils.isDisabled(bot, msg.guild.id, command, command.name);
 			if(!disabled) {
-				return msg.channel.createMessage("Module already enabled.")
+				return msg.channel.createMessage("Command already enabled")
 			} else {
-				dis.commands = dis.commands.filter(x => x != cmd[2]);
+				dis.commands = dis.commands.filter(x => x != command.name);
 				var success = await bot.utils.updateConfig(bot, msg.guild.id, {disabled: dis});
 				if(success) {
 					msg.channel.createMessage("Enabled!")
 				} else {
-					msg.channel.createMessage("Something went wrong.");
+					msg.channel.createMessage("Something went wrong");
 				}
 			}
 		} else {
-			cmd = args[0].toLowerCase()
+			cmd = args.join(" ").toLowerCase()
 			if(["levels", "levelup", "levelups"].includes(cmd)) {
 				dis.levels = false;
 
@@ -36,19 +35,19 @@ module.exports = {
 				if(success) {
 					msg.channel.createMessage("Enabled!")
 				} else {
-					msg.channel.createMessage("Something went wrong.");
+					msg.channel.createMessage("Something went wrong");
 				}
-			} else if(bot.modules[cmd]) {
-				var cmds = Object.keys(bot.commands).filter(c => bot.commands[c].module && bot.commands[c].module == cmd);
-				dis.commands = dis.commands.filter(x => !cmds.includes(x));
+			} else if(bot.modules.get(bot.mod_aliases.get(cmd))) {
+				var mod = bot.modules.get(bot.mod_aliases.get(cmd));
+				dis.commands = dis.commands.filter(x => !mod.commands.get(x));
 				var success = await bot.utils.updateConfig(bot, msg.guild.id, {disabled: dis});
 				if(success) {
 					msg.channel.createMessage("Enabled!")
 				} else {
-					msg.channel.createMessage("Something went wrong.");
+					msg.channel.createMessage("Something went wrong");
 				}
 			} else {
-				return msg.channel.createMessage("Could not enable: "+e);
+				return msg.channel.createMessage("Command/module not found");
 			}
 		}
 		
